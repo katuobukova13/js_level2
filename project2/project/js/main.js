f5455d46c29f6d2738f28c75f1d387781dd45aa5
Kate Obukhova <katuobukhova13@gmail.com>
2021-01-28 23:35:01 +0300

    2 hw

---
+++ project2/project/js/main.js
@@ -0,0 +1,64 @@
+class ProductsList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();
+        this.calculateProducts();
+    }
+
+    _fetchProducts() {
+        this.goods = [
+            { id: 1, title: 'Notebook', price: 2000 },
+            { id: 2, title: 'Mouse', price: 20 },
+            { id: 3, title: 'Keyboard', price: 200 },
+            { id: 4, title: 'Gamepad', price: 50 },
+        ];
+    }
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const productObj = new ProductItem(product);
+            block.insertAdjacentHTML('beforeend', productObj.render())
+        }
+    }
+    //считает стоимость продуктов
+    calculateProducts() {
+        let sum = this.goods.reduce((sum, item) => sum += item.price, 0);
+        console.log(sum);
+    }
+}
+
+class ProductItem {
+    constructor(product, img = 'https://placehold.it/200x150') {
+        this.title = product.title;
+        this.price = product.price;
+        this.id = product.id;
+        this.img = img;
+    }
+
+    render() {
+        return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
+                <button class="buy-btn">Купить</button>
+            </div>`
+    }
+}
+
+class CartList {
+    addItem() { }
+    calculateItems() { }
+    removeItem() { }
+    recalculateOrder() { }
+    render() { }
+}
+
+class CartItem {
+    render() { }
+}
+
+let list = new ProductsList();
+list.render();
+
+
